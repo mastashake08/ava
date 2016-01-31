9aed2613acb0982b3d4e59ea712267a5b9aa0777
Jyrone Parker <jyrone.parker@gmail.com>
2016-01-31 19:44:46 +0000

    added DeviceToken model and migration

---
+++ database/migrations/2016_01_31_193645_create_device_tokens_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeviceTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device_tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('device_tokens');
+    }
+}
