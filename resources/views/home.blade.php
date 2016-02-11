97c5b28390dbb4f07d98fc60c7c90b9e62983fa1
Jyrone Parker <jyrone.parker@gmail.com>
2016-02-11 14:18:14 -0800

    added view to add edit literature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,40 @@
 			</div>
 		</div>
 
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+					<div class="panel-heading">Front Page What We Do</div>
+
+					<div class="panel-body">
+						<form class="form-horizontal" role="form" action="/front-page-what-we-do" method="post">
+							<div class="form-group">
+								<textarea class="form-control" placeholder="Front Page What We Do Literature" name="front_page"></textarea>
+								<button type="submit" class="btn btn-success">Set Literature</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+					<div class="panel-heading">Front Page Intro</div>
+
+					<div class="panel-body">
+						<form class="form-horizontal" role="form" action="/front-page-intro" method="post">
+							<div class="form-group">
+								<textarea class="form-control" placeholder="Front Page Intro Literature" name="front_page"></textarea>
+								<button type="submit" class="btn btn-success">Set Literature</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	@endif
 </div>
 @endsection
