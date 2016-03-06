092e85d6c1cd66ffe2376f738851ce0f8ee63ef9
Jyrone Parker <jyrone.parker@gmail.com>
2016-03-06 19:51:56 +0000

    add page scroll class

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -92,7 +92,7 @@
                     <h2 class="section-heading">{{$frontPage->about_page_header}}</h2>
                     <hr class="light">
                     <p class="text-faded">  {{$frontPage->about_page_body}}</p>
-                    <a href="#contact" class="btn btn-default btn-xl">Get Started!</a>
+                    <a href="#contact" class="page-scroll btn btn-default btn-xl">Get Started!</a>
                 </div>
             </div>
         </div>
